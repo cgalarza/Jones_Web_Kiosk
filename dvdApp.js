8e90b1150b4c8db732797cd731e194b93ff10153
carlagalarza <carla.m.galarza.13@dartmouth.edu>
2014-03-03 17:50:57 -0500

    Cleaned up promotional movies cycle2 carousel.

--- dvdApp.js
+++ dvdApp.js
@@ -2,41 +2,37 @@
 function jonesDvdKiosk(){
 	console.log('ready!');
 	
-	$('.promotional-slideshow').cycle();
 
 	//add new html to promotionalDvds inorder for all the promotional DVDs to be displayed
-	var newHtml = [];
-	var list = ['1', '2', '3', '6', '7', '8'];
+	var accessionNumbers = [1, 2, 3, 6, 7, 8];
 	var titles = ["Animal House", "To Kill a Mockingbird", "Psycho", "West Side Story", "Seven Samurai", "The Seventh Seal"];
 
-	for (var i = 0; i < 6; i++){
-		addMovie(list[i], titles[i]);
-
-	}
-	
+	addingPromotionalMovies(accessionNumbers, titles);
 }
 
-function addMovie(accessionNum, title){
+function addingPromotionalMovies(accessionNumbers, titles){
+	$('.promotional-slideshow').cycle();
 
-	console.log("got to imageResize " + accessionNum);
+		for (var i = 0; i < accessionNumbers.length; i++){
 
-	var img = new Image();
+			console.log(accessionNumbers[i]);
 
-	img.onload = function(){
-		var width = this.width;
-		var height = this.height;
+			var img = new Image();
 
-		var newHeight = 600;
-		var newWidth = (newHeight/height) * width;
+			img.src = '../../Pictures/DVD/' + accessionNumbers[i] + '.jpg';
+			console.log(img.src);
+		
+			var newHeight = 600;
+			img.width = (newHeight/img.height) * img.width;
+			img.height = newHeight;
+			$(img).attr('data-cycle-title', titles[i]);
+			$(img).attr('data-cycle-desc', accessionNumbers[i]);
+		
+			$('.promotional-slideshow').cycle('add', img);
+			$('.promotional-slideshow').cycle('reinit');
+		}
 
-		//$('<img width="' + newWidth + '" height="' + newHeight + '" src="' + imagePath + '" ></img>').appendTo('.promotional-slideshow');
-		var newSlide = '<img width="' + newWidth + '" height="' + newHeight + '" src="' + img.src + '" data-cycle-title="' + title + '" data-cycle-desc="' + accessionNum + '">';
-		$('.promotional-slideshow').cycle('add', newSlide);
-		$('.promotional-slideshow').cycle('reinit');
-
-	};
-
-	img.src = '../../Pictures/DVD/' + accessionNum + '.jpg';
 }
+
 
 $(document).ready(jonesDvdKiosk);
